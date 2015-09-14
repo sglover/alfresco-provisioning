2512860a5ae1eb2878400e1142e5532d9bf1ad2f
Steven Glover <steven.glover@gmail.com>
2015-09-14 20:10:22 +0100

    Remove setup.sh

--- vagrant/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export keypair_path="/Users/sglover/.ssh/sync-key.pem"
-export aws_access_key="AKIAIR7USTD3YIVEYV2A"
-export aws_secret_key="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"
-export ec2_region="eu-west-1"
